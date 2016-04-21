4fcf4c3b75b358abb89b435246fb520d9c29c990
jgbs <joaogabrieldebrittoesilva@gmail.com>
2016-04-21 17:28:03 -0300

    adicionando um mapa simples para iniciar a criacao da tela de add farmarcias

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,10 +4,16 @@ angular.module('starter.controllers', [])
 // .controller('DashCtrl', function($scope) {})
 
 .controller('RemedioCtrl', function($scope, Remedios) {
+
+  $scope.$on('mapInitialized', function (event, map) {
+    $scope.map = map;
+  });
+
   $scope.remedios = Remedios.all();
   $scope.remove = function(remedio) {
     Remedios.remove(remedio);
   };
+  
 })
 
 .controller('AlertaCtrl', function($scope, Alertas) {
